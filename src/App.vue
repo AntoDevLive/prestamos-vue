f99d004108adcb869429a1fb2f2f4700e2014f6f
antoniocp99 <antoniocastizo99@gmail.com>
2025-09-24 11:57:39 +0200

    eventos en vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
   import Header from './components/Header.vue';
+
+  function handleChange(e) {
+    console.log(e.target.value)
+  }
 </script>
 
 <template>
   <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div class="my-5">
+
+
+      <input
+          type="range"
+          className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+          @input="handleChange"
+          
+      />
+    </div>
+
   </div>
 </template>
